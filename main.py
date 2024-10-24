46690fcf738dc6c3cdac0a13bc3a7aef2d10b978
Educarvalhob <edumengocarvalho@gmail.com>
2024-10-23 21:20:41 -0300

    criação dos arquivos product_controller, product_model, product_repository e product_service

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from app.controllers.product_controller import router as product_router
+
+import uvicorn
+
+app = FastAPI()
+
+app.include_router(product_router)
+
+@app.get("/")
+
+def read_root():
+   return {"Hello": "World"}
+
+def main():
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
+if __name__ == "__main__":
+   main()
